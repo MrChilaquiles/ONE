6475b159d10080a8afa117d6da2753b7ae327e6f
MrChilaquiles <RGG2000@gmail.com>
2023-09-09 02:49:54 -0600

    Add new function modify

--- Java/JDBC/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ Java/JDBC/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -1,25 +1,115 @@
 package com.alura.jdbc.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Map;
+import javax.swing.JOptionPane;
+import com.alura.jdbc.factory.ConnectionFactory;
 
 public class ProductoController {
 
-	public void modificar(String nombre, String descripcion, Integer id) {
-		// TODO
+	public int modificar(String nombre, String descripcion, Integer cantidad, Integer id) {
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+			final Connection con = factory.recuperaConexion();
+
+			final PreparedStatement statement = con
+					.prepareStatement("UPDATE PRODUCTO SET NOMBRE = ?, DESCRIPCION = ?, CANTIDAD = ? WHERE ID = ? ");
+			statement.setString(1, nombre);
+			statement.setString(2, descripcion);
+			statement.setInt(3, cantidad);
+			statement.setInt(4, id);
+			statement.execute();
+			JOptionPane.showMessageDialog(null, "Se han modificado los datos");
+			System.out.println(nombre + ", " + descripcion + ", " + cantidad);
+			int updateCount = statement.getUpdateCount();
+			return updateCount;
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			return -1;
+		}
 	}
 
-	public void eliminar(Integer id) {
-		// TODO
+	public int eliminar(Integer id) throws SQLException {
+		ConnectionFactory factory = new ConnectionFactory();
+		final Connection con = factory.recuperaConexion();
+		try (con) {
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
+			try (statement) {
+				statement.setInt(1, id);
+				statement.execute();
+				int updateCount = statement.getUpdateCount();
+				return updateCount;
+			}
+		}
 	}
 
-	public List<?> listar() {
-		// TODO
-		return new ArrayList<>();
+	public List<Map<String, String>> listar() throws SQLException {
+		ConnectionFactory factory = new ConnectionFactory();
+		final Connection con = factory.recuperaConexion();
+		try (con) {
+			final PreparedStatement statement = con
+					.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+			try (statement) {
+				statement.execute();
+				ResultSet resultSet = statement.getResultSet();
+				List<Map<String, String>> resultado = new ArrayList<>();
+				while (resultSet.next()) {
+					Map<String, String> fila = new HashMap<>();
+					fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+					fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+					fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+					fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+					resultado.add(fila);
+				}
+				return resultado;
+			}
+		}
 	}
 
-    public void guardar(Object producto) {
-		// TODO
+	public void guardar(Map<String, String> producto) throws SQLException {
+		String nombre = producto.get("NOMBRE");
+		String descripcion = producto.get("DESCRIPCION");
+		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
+		Integer maxCant = 100;
+		ConnectionFactory factory = new ConnectionFactory();
+		final Connection con = factory.recuperaConexion();
+		try (con) {
+			con.setAutoCommit(false);
+			final PreparedStatement statement = con.prepareStatement(
+					"INSERT INTO PRODUCTO (nombre, descripcion, cantidad) VALUES (?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			try (statement) {
+				do {
+					int cantdidadParaGuardar = Math.min(cantidad, maxCant);
+					ejecutaRegistro(nombre, descripcion, cantdidadParaGuardar, statement);
+					cantidad -= maxCant;
+				} while (cantidad > 0);
+				con.commit();
+			} catch (Exception e) {
+				con.rollback();
+			}
+		}
 	}
 
+	private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
+			throws SQLException {
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, cantidad);
+		statement.execute();
+		final ResultSet resultSet = statement.getGeneratedKeys();
+		try (resultSet) {
+			while (resultSet.next()) {
+				System.out.println(String.format("Fue insertado el valor de ID %d", resultSet.getInt(1)));
+			}
+		}
+	}
 }
--- Java/JDBC/src/main/java/com/alura/jdbc/view/ControlDeStockFrame.java
+++ Java/JDBC/src/main/java/com/alura/jdbc/view/ControlDeStockFrame.java
@@ -4,8 +4,9 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Optional;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -14,243 +15,261 @@ import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
-
 import com.alura.jdbc.controller.CategoriaController;
 import com.alura.jdbc.controller.ProductoController;
 
 public class ControlDeStockFrame extends JFrame {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private JLabel labelNombre, labelDescripcion, labelCantidad, labelCategoria;
-    private JTextField textoNombre, textoDescripcion, textoCantidad;
-    private JComboBox<Object> comboCategoria;
-    private JButton botonGuardar, botonModificar, botonLimpiar, botonEliminar, botonReporte;
-    private JTable tabla;
-    private DefaultTableModel modelo;
-    private ProductoController productoController;
-    private CategoriaController categoriaController;
+	private JLabel labelNombre, labelDescripcion, labelCantidad, labelCategoria;
+	private JTextField textoNombre, textoDescripcion, textoCantidad;
+	private JComboBox<Object> comboCategoria;
+	private JButton botonGuardar, botonModificar, botonLimpiar, botonEliminar, botonReporte;
+	private JTable tabla;
+	private DefaultTableModel modelo;
+	private ProductoController productoController;
+	private CategoriaController categoriaController;
 
-    public ControlDeStockFrame() {
-        super("Productos");
+	public ControlDeStockFrame() {
+		super("Productos");
 
-        this.categoriaController = new CategoriaController();
-        this.productoController = new ProductoController();
+		this.categoriaController = new CategoriaController();
+		this.productoController = new ProductoController();
 
-        Container container = getContentPane();
-        setLayout(null);
+		Container container = getContentPane();
+		setLayout(null);
 
-        configurarCamposDelFormulario(container);
+		configurarCamposDelFormulario(container);
 
-        configurarTablaDeContenido(container);
+		configurarTablaDeContenido(container);
 
-        configurarAccionesDelFormulario();
-    }
+		configurarAccionesDelFormulario();
+	}
 
-    private void configurarTablaDeContenido(Container container) {
-        tabla = new JTable();
+	private void configurarTablaDeContenido(Container container) {
+		tabla = new JTable();
+		tabla.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
+		modelo = (DefaultTableModel) tabla.getModel();
+		modelo.addColumn("Identificador del Producto");
+		modelo.addColumn("Nombre del Producto");
+		modelo.addColumn("Descripción del Producto");
+		modelo.addColumn("Cantidad");
 
-        modelo = (DefaultTableModel) tabla.getModel();
-        modelo.addColumn("Identificador del Producto");
-        modelo.addColumn("Nombre del Producto");
-        modelo.addColumn("Descripción del Producto");
+		cargarTabla();
 
-        cargarTabla();
+		tabla.setBounds(10, 205, 760, 280);
 
-        tabla.setBounds(10, 205, 760, 280);
+		botonEliminar = new JButton("Eliminar");
+		botonModificar = new JButton("Modificar");
+		botonReporte = new JButton("Ver Reporte");
+		botonEliminar.setBounds(10, 500, 80, 20);
+		botonModificar.setBounds(100, 500, 80, 20);
+		botonReporte.setBounds(190, 500, 80, 20);
 
-        botonEliminar = new JButton("Eliminar");
-        botonModificar = new JButton("Modificar");
-        botonReporte = new JButton("Ver Reporte");
-        botonEliminar.setBounds(10, 500, 80, 20);
-        botonModificar.setBounds(100, 500, 80, 20);
-        botonReporte.setBounds(190, 500, 80, 20);
+		container.add(tabla);
+		container.add(botonEliminar);
+		container.add(botonModificar);
+		container.add(botonReporte);
 
-        container.add(tabla);
-        container.add(botonEliminar);
-        container.add(botonModificar);
-        container.add(botonReporte);
+		setSize(800, 600);
+		setVisible(true);
+		setLocationRelativeTo(null);
+	}
 
-        setSize(800, 600);
-        setVisible(true);
-        setLocationRelativeTo(null);
-    }
+	private void configurarCamposDelFormulario(Container container) {
+		labelNombre = new JLabel("Nombre del Producto");
+		labelDescripcion = new JLabel("Descripción del Producto");
+		labelCantidad = new JLabel("Cantidad");
+		labelCategoria = new JLabel("Categoría del Producto");
 
-    private void configurarCamposDelFormulario(Container container) {
-        labelNombre = new JLabel("Nombre del Producto");
-        labelDescripcion = new JLabel("Descripción del Producto");
-        labelCantidad = new JLabel("Cantidad");
-        labelCategoria = new JLabel("Categoría del Producto");
+		labelNombre.setBounds(10, 10, 240, 15);
+		labelDescripcion.setBounds(10, 50, 240, 15);
+		labelCantidad.setBounds(10, 90, 240, 15);
+		labelCategoria.setBounds(10, 130, 240, 15);
 
-        labelNombre.setBounds(10, 10, 240, 15);
-        labelDescripcion.setBounds(10, 50, 240, 15);
-        labelCantidad.setBounds(10, 90, 240, 15);
-        labelCategoria.setBounds(10, 130, 240, 15);
+		labelNombre.setForeground(Color.BLACK);
+		labelDescripcion.setForeground(Color.BLACK);
+		labelCategoria.setForeground(Color.BLACK);
 
-        labelNombre.setForeground(Color.BLACK);
-        labelDescripcion.setForeground(Color.BLACK);
-        labelCategoria.setForeground(Color.BLACK);
+		textoNombre = new JTextField();
+		textoDescripcion = new JTextField();
+		textoCantidad = new JTextField();
+		comboCategoria = new JComboBox<>();
+		comboCategoria.addItem("Elige una Categoría");
+		this.categoriaController.listar();
 
-        textoNombre = new JTextField();
-        textoDescripcion = new JTextField();
-        textoCantidad = new JTextField();
-        comboCategoria = new JComboBox<>();
-        comboCategoria.addItem("Elige una Categoría");
+		textoNombre.setBounds(10, 25, 265, 20);
+		textoDescripcion.setBounds(10, 65, 265, 20);
+		textoCantidad.setBounds(10, 105, 265, 20);
+		comboCategoria.setBounds(10, 145, 265, 20);
 
-        // TODO
-        var categorias = this.categoriaController.listar();
-        // categorias.forEach(categoria -> comboCategoria.addItem(categoria));
+		botonGuardar = new JButton("Guardar");
+		botonLimpiar = new JButton("Limpiar");
+		botonGuardar.setBounds(10, 175, 80, 20);
+		botonLimpiar.setBounds(100, 175, 80, 20);
 
-        textoNombre.setBounds(10, 25, 265, 20);
-        textoDescripcion.setBounds(10, 65, 265, 20);
-        textoCantidad.setBounds(10, 105, 265, 20);
-        comboCategoria.setBounds(10, 145, 265, 20);
+		container.add(labelNombre);
+		container.add(labelDescripcion);
+		container.add(labelCantidad);
+		container.add(labelCategoria);
+		container.add(textoNombre);
+		container.add(textoDescripcion);
+		container.add(textoCantidad);
+		container.add(comboCategoria);
+		container.add(botonGuardar);
+		container.add(botonLimpiar);
+	}
 
-        botonGuardar = new JButton("Guardar");
-        botonLimpiar = new JButton("Limpiar");
-        botonGuardar.setBounds(10, 175, 80, 20);
-        botonLimpiar.setBounds(100, 175, 80, 20);
+	private void configurarAccionesDelFormulario() {
+		botonGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guardar();
+				limpiarTabla();
+				cargarTabla();
+			}
+		});
 
-        container.add(labelNombre);
-        container.add(labelDescripcion);
-        container.add(labelCantidad);
-        container.add(labelCategoria);
-        container.add(textoNombre);
-        container.add(textoDescripcion);
-        container.add(textoCantidad);
-        container.add(comboCategoria);
-        container.add(botonGuardar);
-        container.add(botonLimpiar);
-    }
+		botonLimpiar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiarFormulario();
+			}
+		});
 
-    private void configurarAccionesDelFormulario() {
-        botonGuardar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                guardar();
-                limpiarTabla();
-                cargarTabla();
-            }
-        });
+		botonEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				eliminar();
+				limpiarTabla();
+				cargarTabla();
+			}
+		});
 
-        botonLimpiar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                limpiarFormulario();
-            }
-        });
+		botonModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				modificar();
+//                limpiarTabla();
+//                cargarTabla();
+			}
+		});
 
-        botonEliminar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                eliminar();
-                limpiarTabla();
-                cargarTabla();
-            }
-        });
+		botonReporte.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				abrirReporte();
+			}
+		});
+	}
 
-        botonModificar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                modificar();
-                limpiarTabla();
-                cargarTabla();
-            }
-        });
+	private void abrirReporte() {
+		new ReporteFrame(this);
+	}
 
-        botonReporte.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                abrirReporte();
-            }
-        });
-    }
+	private void limpiarTabla() {
+		modelo.getDataVector().clear();
+	}
 
-    private void abrirReporte() {
-        new ReporteFrame(this);
-    }
+	private boolean tieneFilaElegida() {
+		return tabla.getSelectedRowCount() == 0 || tabla.getSelectedColumnCount() == 0;
+	}
 
-    private void limpiarTabla() {
-        modelo.getDataVector().clear();
-    }
+	private void modificar() {
+		if (tieneFilaElegida()) {
+			JOptionPane.showMessageDialog(this, "Por favor, elije un item");
+			return;
+		}
 
-    private boolean tieneFilaElegida() {
-        return tabla.getSelectedRowCount() == 0 || tabla.getSelectedColumnCount() == 0;
-    }
+		Optional.ofNullable(modelo.getValueAt(tabla.getSelectedRow(), tabla.getSelectedColumn()))
+				.ifPresentOrElse(fila -> {
+					Integer id = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 0).toString());
+					String nombre = (String) modelo.getValueAt(tabla.getSelectedRow(), 1);
+					String descripcion = (String) modelo.getValueAt(tabla.getSelectedRow(), 2);
+					Integer cantidad = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 3).toString());
+					int filasModificadas;
+					try {
+						filasModificadas = this.productoController.modificar(nombre, descripcion, cantidad, id);
+					} catch (Exception e) {
+						e.printStackTrace();
+						throw new RuntimeException(e);
+					}
+					JOptionPane.showMessageDialog(this,
+							String.format("%d item modificado con éxito!", filasModificadas));
+				}, () -> JOptionPane.showMessageDialog(this, "Por favor, elije un item"));
+	}
 
-    private void modificar() {
-        if (tieneFilaElegida()) {
-            JOptionPane.showMessageDialog(this, "Por favor, elije un item");
-            return;
-        }
+	private void eliminar() {
+		if (tieneFilaElegida()) {
+			JOptionPane.showMessageDialog(this, "Por favor, elije un item");
+			return;
+		}
 
-        Optional.ofNullable(modelo.getValueAt(tabla.getSelectedRow(), tabla.getSelectedColumn()))
-                .ifPresentOrElse(fila -> {
-                    Integer id = (Integer) modelo.getValueAt(tabla.getSelectedRow(), 0);
-                    String nombre = (String) modelo.getValueAt(tabla.getSelectedRow(), 1);
-                    String descripcion = (String) modelo.getValueAt(tabla.getSelectedRow(), 2);
+		Optional.ofNullable(modelo.getValueAt(tabla.getSelectedRow(), tabla.getSelectedColumn()))
+				.ifPresentOrElse(fila -> {
+					Integer id = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 0).toString());
+					int cantidadEliminada;
+					try {
+						cantidadEliminada = this.productoController.eliminar(id);
+					} catch (SQLException e) {
+						throw new RuntimeException(e);
+					}
 
-                    this.productoController.modificar(nombre, descripcion, id);
-                }, () -> JOptionPane.showMessageDialog(this, "Por favor, elije un item"));
-    }
+					modelo.removeRow(tabla.getSelectedRow());
 
-    private void eliminar() {
-        if (tieneFilaElegida()) {
-            JOptionPane.showMessageDialog(this, "Por favor, elije un item");
-            return;
-        }
+					JOptionPane.showMessageDialog(this, cantidadEliminada + " item eliminado con éxito!");
+				}, () -> JOptionPane.showMessageDialog(this, "Por favor, elije un item"));
+	}
 
-        Optional.ofNullable(modelo.getValueAt(tabla.getSelectedRow(), tabla.getSelectedColumn()))
-                .ifPresentOrElse(fila -> {
-                    Integer id = (Integer) modelo.getValueAt(tabla.getSelectedRow(), 0);
+	private void cargarTabla() {
+		try {
+			var productos = this.productoController.listar();
 
-                    this.productoController.eliminar(id);
+			try {
+				productos.forEach(producto -> modelo.addRow(new Object[] { producto.get("ID"), producto.get("NOMBRE"),
+						producto.get("DESCRIPCION"), producto.get("CANTIDAD") }));
+			} catch (Exception e) {
+				throw e;
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-                    modelo.removeRow(tabla.getSelectedRow());
+	private void guardar() {
+		if (textoNombre.getText().isBlank() || textoDescripcion.getText().isBlank()) {
+			JOptionPane.showMessageDialog(this, "Los campos Nombre y Descripción son requeridos.");
+			return;
+		}
 
-                    JOptionPane.showMessageDialog(this, "Item eliminado con éxito!");
-                }, () -> JOptionPane.showMessageDialog(this, "Por favor, elije un item"));
-    }
+		Integer cantidadInt;
 
-    private void cargarTabla() {
-        var productos = this.productoController.listar();
+		try {
+			cantidadInt = Integer.parseInt(textoCantidad.getText());
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(this, String
+					.format("El campo cantidad debe ser numérico dentro del rango %d y %d.", 0, Integer.MAX_VALUE));
+			return;
+		}
 
-        try {
-            // TODO
-            // productos.forEach(producto -> modelo.addRow(new Object[] { "id", "nombre",
-            // "descripcion" }));
-        } catch (Exception e) {
-            throw e;
-        }
-    }
+		var producto = new HashMap<String, String>();
+		producto.put("NOMBRE", textoNombre.getText());
+		producto.put("DESCRIPCION", textoDescripcion.getText());
+		producto.put("CANTIDAD", String.valueOf(cantidadInt));
+		comboCategoria.getSelectedItem();
 
-    private void guardar() {
-        if (textoNombre.getText().isBlank() || textoDescripcion.getText().isBlank()) {
-            JOptionPane.showMessageDialog(this, "Los campos Nombre y Descripción son requeridos.");
-            return;
-        }
+		try {
+			this.productoController.guardar(producto);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 
-        Integer cantidadInt;
+		JOptionPane.showMessageDialog(this, "Registrado con éxito!");
 
-        try {
-            cantidadInt = Integer.parseInt(textoCantidad.getText());
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(this, String
-                    .format("El campo cantidad debe ser numérico dentro del rango %d y %d.", 0, Integer.MAX_VALUE));
-            return;
-        }
+		this.limpiarFormulario();
+	}
 
-        // TODO
-        var producto = new Object[] { textoNombre.getText(), textoDescripcion.getText(), cantidadInt };
-        var categoria = comboCategoria.getSelectedItem();
-
-        this.productoController.guardar(producto);
-
-        JOptionPane.showMessageDialog(this, "Registrado con éxito!");
-
-        this.limpiarFormulario();
-    }
-
-    private void limpiarFormulario() {
-        this.textoNombre.setText("");
-        this.textoDescripcion.setText("");
-        this.textoCantidad.setText("");
-        this.comboCategoria.setSelectedIndex(0);
-    }
+	private void limpiarFormulario() {
+		this.textoNombre.setText("");
+		this.textoDescripcion.setText("");
+		this.textoCantidad.setText("");
+		this.comboCategoria.setSelectedIndex(0);
+	}
 
 }
